23686a843878c84d1e2b677bb1cb1c3dc8bdd2d5
LuLuLuann <lu.akaydin@gmail.com>
2025-02-11 22:35:23 -0500

    add files and logic -- not working at this point

--- index.js
+++ index.js
@@ -30,8 +30,64 @@
 
 // Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
 
-// Commit frequently to the git repository. (3x minimum)
+// Commit frequently to the git repository. (3x minimum) -- 1x done 2/9 3am
 
 // Include a README file that contains a description of your application.
 
 // Level of effort displayed in creativity, presentation, and user experience.
+
+
+
+// const allDivsWithClassName = document.getElementsByClassName("keys");
+
+// Array.from(allDivsWithClassName).forEach(element => {
+//     element.addEventListener('keydown', handleKeyPress);
+// });
+
+// // turned keyCode into "key" bc "keyCode" is deprecated
+// document.addEventListener("keydown", function (event) {
+//     console.log(event.key);
+//     const audio = document.querySelector(`audio[data-key="${event.key}"]`);
+//     console.log(audio);
+//     if (audio) {
+//         audio.play();
+//     }
+// });
+
+
+// function handleKeyPress(event) {
+
+//     if (event.key === 'A') {
+//         // Do something when Enter key is pressed
+//       }
+//     }
+  
+   
+//   // Add event listener to an element
+  
+
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; // stops the function from running
+audio.currentTime=0; // rewinds to the start
+    audio.play();
+    key.classList.add("playing");
+
+    console.log(key);
+};
+
+function removeTransition(e){
+console.log(e);
+if (e.propertyName !== "transition") return; // skip if it's not a transform
+console.log(e.propertyName);
+this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+
+// listen for an event called transition end on each one (bc you cannot listen on an array -- you have to loop over)
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
